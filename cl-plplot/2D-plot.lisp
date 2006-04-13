c8204a5519b67a2e3a1b234d915b23df57912338
hbabcock <>
2006-04-13 02:21:21 +0000

    Added the ability to make simple bar graphs

--- cl-plplot/2D-plot.lisp
+++ cl-plplot/2D-plot.lisp
@@ -31,6 +31,8 @@
 ;;;; hazen 3/06
 ;;;;
 
+(in-package #:cl-user)
+
 (defpackage #:cl-plplot
   (:use #:common-lisp
 	#:cl-plplot-system)
@@ -42,6 +44,7 @@
 	   #:set-axis-ticks
 	   #:set-axis-format
 	   #:x-y-plot
+	   #:bar-graph
 	   #:get-cursor
 	   #:set-axis-labels
 	   #:set-device&size
@@ -61,7 +64,7 @@
   (yopt "bcnst"))
 
 (defparameter *options* nil)
-
+(defvar default-bar-colors nil)
 
 ;;; helpers
 
@@ -88,6 +91,13 @@
 	 (format t "Unrecognized color: ~A~%" symbol)
 	 (vector 0 0 0)))))
 
+(defun init-default-bar-colors ()
+  "Initializes default-bar-colors"
+  (setf default-bar-colors 
+	(list :blue :red :yellow :green :magenta :white :brown :grey)))
+
+(init-default-bar-colors)
+
 (defun min-max (vector)
   "Returns the minimum and maximum of vector"
   (let ((min (aref vector 0))
@@ -100,8 +110,30 @@
 	(setf min (aref vector i))))
     (values min max)))
 
+(defun bar-graph-min-max (data side-by-side)
+  "Figures out what the maximum value is in the data that the
+   user supplied for making a bar-graph"
+  (let ((min (aref data 0 0))
+	(max (aref data 0 0)))
+    (if side-by-side
+	(dotimes (i (array-dimension data 0))
+	  (dotimes (j (array-dimension data 1))
+	    (when (< (aref data i j) min) (setf min (aref data i j)))
+	    (when (> (aref data i j) max) (setf max (aref data i j)))))
+	(dotimes (i (array-dimension data 0))
+	  (let ((sum 0))
+	    (dotimes (j (array-dimension data 1))
+	      (incf sum (aref data i j)))
+	    (when (< sum min) (setf min sum))
+	    (when (> sum max) (setf max sum)))))
+    (let ((diff (* 0.05 (- max min))))
+      (cond
+	((< (abs max) diff) (values (- min diff) 0.0))
+	((< (abs min) diff) (values 0.0 (+ max diff)))
+	(t (values (- min diff) (+ max diff)))))))
+
 (defmacro check-min-max (min max vec)
-  "Sets min and max from vec unless they are already both defined"
+  "Sets min and max from vec unless they are already *both* defined"
   (let ((g0 (gensym))
 	(g1 (gensym))
 	(g2 (gensym)))
@@ -130,12 +162,13 @@
      (t (format t "Malformed color option: ~A~%" ,color))))
 
 (defun set-color (which vec &optional (change-pen t))
-  "Sets the color..."
+  "Sets the current pen color"
   (plscol0 which (aref vec 0) (aref vec 1) (aref vec 2))
   (when change-pen
     (plcol0 which)))
 
 (defun len-check (x y x-error y-error)
+  "Checks that the arrays we have been given are appropriately defined"
   (let ((good-dims t)
 	(x-length (length x)))
     (unless (= x-length (length y))
@@ -150,6 +183,25 @@
       (format t "x & y-error do not have the same number of elements!~%")
       (setf good-dims nil))
     good-dims))
+
+(defun setup-plotting-window (xmin xmax ymin ymax)
+  "Initializes the plotting window"
+  (set-color 0 (options-background-color *options*) nil)
+  (plinit)
+  (set-color 1 (options-foreground-color *options*))
+  (pladv 0)
+  (plvpor 0.1 0.9 0.1 0.9)
+  (plwind xmin xmax ymin ymax)
+  (plbox (options-xopt *options*) (options-xtick *options*) (options-nxsub *options*)
+	 (options-yopt *options*) (options-ytick *options*) (options-nysub *options*)))
+
+(defun set-user-specd-color (color)
+  "Sets the pen color appropriately based on what the user supplied as a color"
+  (cond
+    ((and color (symbolp color)) (set-color 1 (symbol-to-color color)))
+    ((and color (vectorp color) (= 3 (length color))) (set-color 1 color))
+    (t (set-color 1 (options-foreground-color *options*)))))
+
 
 ;;; exported
 
@@ -264,7 +316,7 @@
        xmin, xmax, ymin & ymax can be used to specify the axis ranges on the *first* call to this
           function. If they are not specified then default values are chosen based on x & y.
        line should be an integer line width, or zero if no line is desired
-       line-style specified what style line to draw (if a line is drawn), this should be a number
+       line-style specifies what style line to draw (if a line is drawn), this should be a number
           between 1 and 8.
        symbol-type should be set to a number (that specifies a symbol) if you want symbols plotted.
        color is the color to use when plotting the lines and symbols, it should be a symbol
@@ -277,18 +329,8 @@
       (setf initialized-plot t)
       (check-min-max xmin xmax x)
       (check-min-max ymin ymax y)
-      (set-color 0 (options-background-color *options*) nil)
-      (plinit)
-      (set-color 1 (options-foreground-color *options*))
-      (pladv 0)
-      (plvpor 0.1 0.9 0.1 0.9)
-      (plwind xmin xmax ymin ymax)
-      (plbox (options-xopt *options*) (options-xtick *options*) (options-nxsub *options*)
-	     (options-yopt *options*) (options-ytick *options*) (options-nysub *options*)))
-    (cond
-      ((and color (symbolp color)) (set-color 1 (symbol-to-color color)))
-      ((and color (vectorp color) (= 3 (length color))) (set-color 1 color))
-      (t (set-color 1 (options-foreground-color *options*))))
+      (setup-plotting-window xmin xmax ymin ymax))
+    (set-user-specd-color color)
     (when (len-check x y x-error y-error)
       (when (> line 0)
 	(when (and (> line-style 0)
@@ -307,7 +349,66 @@
 	  (plerrx min-error max-error y)))
       (when y-error
 	(multiple-value-bind (min-error max-error) (error-bar y y-error)
-	  (plerry x min-error max-error))))))
+	  (plerry x min-error max-error)))))
+  
+  (defun bar-graph (data &key (x-data nil) (ymin nil) (ymax nil) (line 1) (filled t) (side-by-side nil) (colors default-bar-colors))
+    "Draws a bar graph. Assumes a uniform spacing of the data in x.
+     Arguments:
+       data a 1 or 2 dimensional array the specifies the heights of the bars. In the case of
+          a 2D array, the second dimension specifies the bar heights for each grouping.
+       x-data is a 1D array that specifies the locations of the bars. If this is nil a 
+          default integer spacing is assumed.
+       ymin, ymax specify the axis ranges on the *first* call to this function. If they are
+          not specified then default values are chosen based on data.
+       line should be an integer line width, or zero if no line around the box is desired.
+       filled (t or nil) specifies whether the bars should be filled.
+       side-by-side (t or nil) specified whether the bars in a group should be stacked or
+          drawn side-by-side.
+       colors is a list of symbols and/or RGB 3-vectors that specifies what color each of
+          the individual bars in a group should be colored."
+    (when (= (length (array-dimensions data)) 1)
+      (let ((tmp (make-array (list (length data) 1) :initial-element 0.0 :element-type 'float)))
+	(dotimes (i (length data))
+	  (setf (aref tmp i 0) (aref data i)))
+	(setf data tmp)))
+    (unless initialized-plot
+      (setf initialized-plot t)
+      (let ((xmin -0.5)
+	    (xmax (- (array-dimension data 0) 0.5)))
+	(when (and x-data
+		   (vectorp x-data))
+	  (let ((tmp (* 0.5 (- (aref x-data 1) (aref x-data 0)))))
+	    (setf xmin (- (aref x-data 0) tmp))
+	    (setf xmax (+ (aref x-data (1- (length x-data))) tmp))))
+	(if (and ymin ymax)
+	    (setup-plotting-window xmin xmax ymin ymax)
+	    (multiple-value-bind (new-ymin new-ymax) (bar-graph-min-max data side-by-side)
+	      (setup-plotting-window xmin xmax new-ymin new-ymax)))))
+    (unless (and x-data
+		 (= (length x-data) (array-dimension data 0)))
+      (setf x-data (make-array (array-dimension data 0) :initial-element 0.0 :element-type 'float))
+      (dotimes (i (array-dimension data 0))
+	(setf (aref x-data i) i)))
+    (when (= (length x-data) (array-dimension data 0))
+      (let* ((bin-width (if (> (length x-data) 1) (- (aref x-data 1) (aref x-data 0)) 1.0))
+	     (half-width (* 0.5 bin-width))
+	     (bar-width (* 0.75 (if side-by-side (/ bin-width (array-dimension data 1)) bin-width))))
+	(dotimes (i (array-dimension data 0))
+	  (let ((start-x (+ (aref x-data i) (* 0.125 bar-width) (- half-width)))
+		(cur-y 0.0))
+	    (dotimes (j (array-dimension data 1))
+	      (let ((x (vector start-x start-x (+ start-x bar-width) (+ start-x bar-width)))
+		    (y (vector cur-y (+ cur-y (aref data i j)) (+ cur-y (aref data i j)) cur-y)))
+		(when filled
+		  (set-user-specd-color (elt colors (mod j (length colors))))
+		  (plfill x y))
+		(when (> line 0)
+		  (set-color 1 (options-foreground-color *options*))
+		  (plwid line)
+		  (plline x y)))
+	      (if side-by-side
+		  (incf start-x (* (/ 1.0 0.75) bar-width))
+		  (incf cur-y (aref data i j))))))))))
 
 (defun get-cursor ()
   "Returns the x & y coordinates (as defined by the current plot) of the next mouse click.
