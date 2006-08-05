3b4e57817386a493b15b4eca294c30411521becf
hbabcock <>
2006-08-05 00:20:25 +0000

    Changed text handling so that it would handle utf8 encoded strings properly, though not perhaps optimally.

--- cl-plplot/src/window/text-item.lisp
+++ cl-plplot/src/window/text-item.lisp
@@ -1,25 +1,4 @@
 ;;;;
-;;;; Copyright (c) 2006 Hazen P. Babcock
-;;;;
-;;;; Permission is hereby granted, free of charge, to any person obtaining a copy 
-;;;; of this software and associated documentation files (the "Software"), to 
-;;;; deal in the Software without restriction, including without limitation the 
-;;;; rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-;;;; sell copies of the Software, and to permit persons to whom the Software is 
-;;;; furnished to do so, subject to the following conditions:
-;;;;
-;;;; The above copyright notice and this permission notice shall be included in 
-;;;; all copies or substantial portions of the Software.
-;;;;
-;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-;;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-;;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-;;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-;;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-;;;; IN THE SOFTWARE.
-;;;;
-;;;;
 ;;;; Functions that are most closely related to the text-item class.
 ;;;;
 ;;;; hazen 6/06
@@ -86,7 +65,44 @@
 
 (defmethod render-text ((a-text-item text-item))
   "Sets the current color and font size to those specified by the
-   text item. Returns a string contained in the text item."
+   text item. Processes the possibly utf-8 encoded string contained 
+   in text item to properly handle non ASCII characters. Returns
+   the processed string."
   (set-foreground-color (text-color a-text-item))
   (plschr 0 (font-size a-text-item))
-  (the-text a-text-item))
+  (let* ((utf8-text (the-text a-text-item))
+	 (escaped-string (make-array (* 10 (length utf8-text)) :element-type 'character))
+	 (l 0))
+    (dotimes (i (length utf8-text))
+      (let ((code (char-code (char utf8-text i))))
+	(if (> code 128)
+	  (let ((converted-char (format nil "#[~A]" code)))
+	    (dotimes (j (length converted-char))
+	      (setf (char escaped-string l) (char converted-char j))
+	      (incf l)))
+	  (progn
+	    (setf (char escaped-string l) (char utf8-text i))
+	    (incf l)))))
+    (subseq escaped-string 0 l)))
+
+;;;;
+;;;; Copyright (c) 2006 Hazen P. Babcock
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person obtaining a copy 
+;;;; of this software and associated documentation files (the "Software"), to 
+;;;; deal in the Software without restriction, including without limitation the 
+;;;; rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+;;;; sell copies of the Software, and to permit persons to whom the Software is 
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be included in 
+;;;; all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+;;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+;;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+;;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+;;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+;;;; IN THE SOFTWARE.
+;;;;
