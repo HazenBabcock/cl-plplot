9b69a599d8c44eaa6e6baed5136d84ea1f8f5baa
Hazen Babcock <hbabcock@mac.com>
2014-02-01 00:06:04 -0500

    Initial commit of a new macro for creating PLplot function calls.

--- src/system/pl-defcfun.lisp
+++ src/system/pl-defcfun.lisp
@@ -43,36 +43,79 @@
 
 (in-package #:cl-plplot-system)
 
-(defun parse-args (args)
-  "Parses an argument list as part of the process of figuring out how to wrap a function."
-  (let ((length-args (make-hash-table :test 'equal)))
+(defun make-defcfun-args (args)
+  "Returns the args list in a format suitable for calling the function created by defcfun."
+  (let ((defcfun-args nil))
     (dolist (arg args)
-      (when (> (length arg) 2)
-	(if (listp (elt arg 2))
-	    (dolist (var (elt arg 2))
-	      (setf (gethash var length-args) (car arg))
-  (let ((wrapper-fn-args (list))
-	(defcfun-call-args (list))
-	(defcfun-args (list)))
+      (push (list (car arg) (cadr arg)) defcfun-args))
+    (nreverse defcfun-args)))
+
+(defun make-wrapper-args (args)
+  "Returns the args list as they will appear in the wrapper function."
+  (let ((wrapper-args nil))
     (dolist (arg args)
-      (
+      (when (= (length arg) 2)
+	(push (car arg) wrapper-args)))
+    (nreverse wrapper-args)))
+
+(defun make-wrapper-check (args)
+  "Parse args list to determine what checks need to be made on array lengths."
+  (let ((wrapper-check (list 'and)))
+    (dolist (arg args)
+      (when (= (length arg) 3)
+	(let ((elt-2 (elt arg 2)))
+	  (when (listp elt-2)
+	    (let ((ck-list (list '=)))
+	      (dotimes (i (1- (length elt-2)))
+		(push `,(substitute (elt elt-2 (1+ i)) 'x (car elt-2)) ck-list))
+	      (push (nreverse ck-list) wrapper-check))))))
+    (nreverse wrapper-check)))
+
+(defun make-wrapper-return (args)
+  "Parse args list to make local variables that will be returned."
+  (let ((wrapper-return nil))
+    (dolist (arg args)
+      (when (and (= (length arg) 3)
+		 (numberp (elt arg 2)))
+	(push (car arg) wrapper-return)))
+    (nreverse wrapper-return)))
+
+(defun make-wrapper-vars (args)
+  "Parse args list to make local variables needed in the wrapper function."
+  (let ((wrapper-vars nil))
+    (dolist (arg args)
+      (when (= (length arg) 3)
+	(let ((elt-2 (elt arg 2)))
+	  (cond
+	    ((numberp elt-2)
+	     (push (list (car arg) `(make-array ,elt-2)) wrapper-vars))
+	    ((listp elt-2)
+	     (push (list (car arg) `,(substitute (elt elt-2 1) 'x (car elt-2))) wrapper-vars))))))
+    (nreverse wrapper-vars)))
 
 (defmacro pl-defcfun (name returns &rest args)
   "Function creation macro, wraps defcfun to handle most styles of function call in the plplot library"
   (let ((c-name (car name))
 	(lisp-name (cadr name)))
     (export lisp-name)
-    (if (should-wrap args)
-	(let ((wrapped-name (list name
+    (if (should-wrap? args)
+	(let ((wrapped-name (list (car name)
 				  (read-from-string (concatenate 'string "c-" (string lisp-name))))))
-	  (export wrapped-name)
-	  `(defcfun ,wrapped-name ,returns ,@args))
+	  `(defun ,lisp-name ,(make-wrapper-args args)
+	     (let ,(make-wrapper-vars args)
+	       (if ,(make-wrapper-check args)
+		   (progn
+		     (defcfun ,wrapped-name ,returns ,@(make-defcfun-args args))
+		     (values ,@(make-wrapper-return args)))
+		   (format t "Input array sizes do not match!~%")))))
 	`(defcfun (,c-name ,lisp-name) ,returns ,@args))))
 
-(defun should-wrap (args)
+(defun should-wrap? (args)
   "Based on the argument list, determines if the function is complicated enough to need a wrapper."
+  (format t "sw~%")
   (dolist (arg args)
     (when (> (length arg) 2)
+      (format t "swt~%")
       (return t))))
 
 ;;;;
