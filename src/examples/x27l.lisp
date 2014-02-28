ea492f8725ec9f02e4c675857762e6d156d0d072
Hazen Babcock <hbabcock@mac.com>
2014-02-27 22:13:03 -0500

    Update to PLplot 5.10

--- src/examples/x27l.lisp
+++ src/examples/x27l.lisp
@@ -1,20 +1,22 @@
 ;;;;
 ;;;; PLplot example 27
 ;;;;
-;;;; hazen 07/10
+;;;; hazen 02/14
 ;;;;
 
 (in-package :plplot-examples)
 
 (defun example27 (&optional (dev default-dev))
   (plsdev dev)
-  (labels ((spiro (params)
+  (plsetopt "eofill" "")
+  (labels ((spiro (params fill)
 	     (let* ((npnt 20000)
 		    (xcoord (make-float-array (1+ npnt)))
 		    (ycoord (make-float-array (1+ npnt)))
-		    (windings (floor (aref params 3)))
+		    (windings (floor (/ (abs (floor (aref params 1)))
+					(gcd (floor (aref params 0)) (floor (aref params 1))))))
 		    (steps (floor (/ npnt windings)))
-		    (dphi (/ (* 8.0 (acos -1.0)) steps)))
+		    (dphi (* 2.0 (/ 3.14159 (float steps)))))
 	       (dotimes (i (1+ (* windings steps)))
 		 (let* ((dp (- (aref params 0) (aref params 1)))
 			(phi (* i dphi))
@@ -25,13 +27,34 @@
 					    (* (aref params 2) (sin phiw))))))
 	       (multiple-value-bind (xmin xmax) (min-max xcoord)
 		 (multiple-value-bind (ymin ymax) (min-max ycoord)
-		   (let ((scale (if (> (- xmax xmin) (- ymax ymin))
-				    (- xmax xmin)
-				    (- ymax ymin))))
-		     (plwind (* -0.65 scale) (* 0.65 scale) (* -0.65 scale) (* 0.65 scale))
+		   (let ((xrange-adjust (* 0.15 (- xmax xmin)))
+			 (yrange-adjust (* 0.15 (- ymax ymin))))
+		     (plwind (- xmin xrange-adjust) (+ xmax xrange-adjust) 
+			     (- ymin yrange-adjust) (+ ymax yrange-adjust))
 		     (plcol0 1)
-		     (plline (adjust-array xcoord (1+ (* windings steps)))
-			     (adjust-array ycoord (1+ (* windings steps))))))))))
+		     (if fill
+			 (plfill (adjust-array xcoord (1+ (* windings steps)))
+				 (adjust-array ycoord (1+ (* windings steps))))
+			 (plline (adjust-array xcoord (1+ (* windings steps)))
+				 (adjust-array ycoord (1+ (* windings steps))))))))))
+	   (arc ()
+	     (let* ((nseg 8)
+		    (theta 0.0)
+		    (dtheta (/ 360.0 (float nseg))))
+	       (plenv -10.0 10.0 -10.0 10.0 1 0)
+	       (dotimes (i nseg)
+		 (plcol0 (+ (mod i 2) 1))
+		 (plarc 0.0 0.0 8.0 8.0 theta (+ theta dtheta) 0.0 nil)
+		 (incf theta dtheta))
+	       (let* ((a 3.0)
+		      (b (* a (tan (/ (/ (* dtheta 3.14159) 180.0) 2.0))))
+		      (theta (/ dtheta 2.0)))
+		 (dotimes (i nseg)
+		   (plcol0 (- 2 (mod i 2)))
+		   (plarc (* a (cos (/ (* theta 3.14159) 180.0))) (* a (sin (/ (* theta 3.14159) 180.0)))
+			  a b 0.0 360.0 theta 1)
+		   (incf theta dtheta))))))
+			   
     (let ((params (vector (vector 21.0 7.0 7.0 3.0)
 			  (vector 21.0 7.0 10.0 3.0)
 			  (vector 21.0 -7.0 10.0 3.0)
@@ -46,18 +69,27 @@
       (dotimes (i 9)
 	(pladv 0)
 	(plvpor 0.0 1.0 0.0 1.0)
-	(spiro (aref params i)))
+	(spiro (aref params i) nil))
 
       (pladv 0)
       (plssub 1 1)
       (dotimes (i 9)
 	(pladv 0)
 	(plvpor 0.0 1.0 0.0 1.0)
-	(spiro (aref params i)))
+	(spiro (aref params i) nil))
+
+      (pladv 0)
+      (plssub 1 1)
+      (dotimes (i 9)
+	(pladv 0)
+	(plvpor 0.0 1.0 0.0 1.0)
+	(spiro (aref params i) t))
+
+      (arc)
       (plend1))))
 
 ;;;;
-;;;; Copyright (c) 2010 Hazen P. Babcock
+;;;; Copyright (c) 2014 Hazen P. Babcock
 ;;;;
 ;;;; Permission is hereby granted, free of charge, to any person obtaining a copy 
 ;;;; of this software and associated documentation files (the "Software"), to 
