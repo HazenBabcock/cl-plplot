4f198e73df01a2f1a4073ae890ffb8ea6f42cb59
Hazen Babcock <hbabcock@mac.com>
2014-12-12 21:59:57 -0500

    Curves and histogram working. Menubar does not resize properly.. No interactive selection.

--- cl-cffi-gtk-plot/gtk-example.lisp
+++ cl-cffi-gtk-plot/gtk-example.lisp
@@ -14,48 +14,133 @@
 	:glib 
 	:gio 
 	:pango 
-	:cairo 
+	:cairo
+	:cl-plplot-system
 	:common-lisp))
 
 (in-package :cl-cffi-gtk-plot)
 
-(defun create-menus (window box)
-  (let ((menu-bar (gtk-menu-bar-new))
-	(plot-item (gtk-menu-item-new-with-label "Plot")))
-    (gtk-box-pack-start box menu-bar)
-    (gtk-menu-shell-append menu-bar plot-item)
-    (let ((plot-menu (gtk-menu-new)))
-      (setf (gtk-menu-item-submenu plot-item) plot-menu)
-      (let ((curves-item (gtk-menu-item-new-with-label "Curves"))
-	    (histogram-item (gtk-menu-item-new-with-label "Histogram"))
-	    (interactive-item (gtk-menu-item-new-with-label "Interactive Selection"))
-	    (quit-item (gtk-menu-item-new-with-label "Quit")))
-	(gtk-menu-shell-append plot-menu curves-item)
-	(gtk-menu-shell-append plot-menu histogram-item)
-	(gtk-menu-shell-append plot-menu interactive-item)
-	(gtk-menu-shell-append plot-menu (gtk-separator-menu-item-new))
-	(gtk-menu-shell-append plot-menu quit-item)
-	(g-signal-connect quit-item "activate"
-			  (lambda (widget)
-			    (declare (ignore widget))
-			    (gtk-widget-destroy window)))))))
+(defun make-float-array (length &optional (initial-value 0.0))
+  (make-array length :element-type 'float :initial-element initial-value))
 
-(defun plot (window cr)
+(defun plot-curves (window cr)
   ;; Clear surface
-  (cairo-set-source-rgb cr 1.0 1.0 1.0)
+  (cairo-set-source-rgb cr 0 0 0)
   (cairo-paint cr)
-  ;; Example is in 1.0 x 1.0 coordinate space
-  (cairo-scale cr
-	       (gdk-window-get-width window)
-	       (gdk-window-get-height window))
-  ;; Drawing code goes here
-  (cairo-set-line-width cr 0.1)
-  (cairo-set-source-rgb cr 1.0 0.0 0.0)
-  (cairo-rectangle cr 0.25 0.25 0.5 0.5)
-  (cairo-stroke cr)
+
+  ; Initialize PLplot extcairo plotting device.
+  (plsdev "extcairo")
+  (plspage 0 0 (gdk-window-get-width window) (gdk-window-get-height window) 0 0)
+  (plinit)
+  (pl-cmd 26 cr)
+
+  ; 1st plot
+  (let ((x (make-float-array 360))
+	(s (make-float-array 360))
+	(c (make-float-array 360)))
+    (dotimes (i 360)
+      (setf (aref x i) i)
+      (setf (aref s i) (sin (/ (* 3.14159 i) 180.0)))
+      (setf (aref c i) (cos (/ (* 3.14159 i) 180.0))))
+    (pladv 0)
+    (plvpor 0.05 0.95 0.05 0.45)
+    (plwind 0.0 360.0 -1.2 1.2)
+
+    (plcol0 2)
+    (plbox "bcnst" 0 0 "bcnst" 0 0)
+
+    (plcol0 1)
+    (plwidth 2)
+    (plline x s)
+
+    (plcol0 3)
+    (plwidth 1)
+    (pllsty 2)
+    (plline x c)
+    (pllsty 1)
+
+    (plcol0 2)
+    (plmtex "t" 1.0 0.5 0.5 "Sines"))
+
+  ; 2nd plot
+  (let ((x (make-float-array 201))
+	(s (make-float-array 201))
+	(c (make-float-array 201)))
+    (dotimes (i 201)
+      (setf (aref x i) (+ -1.0 (/ i 100.0)))
+      (setf (aref s i) (* (aref x i) (aref x i)))
+      (setf (aref c i) (* (aref s i) (aref x i))))
+    (plvpor 0.05 0.95 0.55 0.95)
+    (plwind -1.0 1.0 -1.0 1.0)
+    
+    (plcol0 2)
+    (plbox "bcnst" 0 0 "bcnst" 0 0)
+
+    (plcol0 1)
+    (plwidth 2)
+    (plline x s)
+
+    (plcol0 3)
+    (plwidth 1)
+    (pllsty 2)
+    (plline x c)
+    (pllsty 1)
+
+    (plcol0 2)
+    (plmtex "t" 1.0 0.5 0.5 "Square & Cubic"))
+
+  (plend)
+
   ;; Destroy the Cario context
-  (cairo-destroy cr)
-  t)
+  (cairo-destroy cr))
+
+(defun plot-histogram (window cr)
+  ;; Clear surface
+  (cairo-set-source-rgb cr 0 0 0)
+  (cairo-paint cr)
+
+  ; Initialize PLplot extcairo plotting device.
+  (plsdev "extcairo")
+  (plspage 0 0 (gdk-window-get-width window) (gdk-window-get-height window) 0 0)
+  (plinit)
+  (pl-cmd 26 cr)
+
+  (labels ((plfbox (x0 y0)
+	     (let ((x (vector x0 x0 (1+ x0) (1+ x0)))
+		   (y (vector 0.0 y0 y0 0.0)))
+	       (plfill x y)
+	       (plcol0 1)
+	       (pllsty 1)
+	       (plline x y))))
+    (let ((y0 (vector 5 15 12 24 28 30 20 8 12 3))
+	  (pos (vector 0.0 0.25 0.5 0.75 1.0))
+	  (red (vector 0.0 0.25 0.5 1.0 1.0))
+	  (green (vector 1.0 0.5 0.5 0.5 1.0))
+	  (blue (vector 1.0 1.0 0.5 0.25 0.0)))
+
+      (pladv 0)
+      (plvsta)
+      
+      (plcol0 2)
+
+      (plwind 1980.0 1990.0 0.0 35.0)
+      (plbox "bc" 1.0 0 "bcnv" 10.0 0)
+      (plcol0 2)
+      (pllab "Year" "Widget Sales (millions)" "#frPLplot Example 12")
+
+      (plscmap1l 1 pos red green blue nil)
+      
+      (dotimes (i 10)
+	(plcol1 (/ i 9.0))
+	(plpsty 0)
+	(plfbox (+ 1980 i) (aref y0 i))
+	(plptex (+ 1980 i 0.5) (1+ (aref y0 i)) 1.0 0.0 0.5 (format nil "~,0f" (aref y0 i)))
+	(plmtex "b" 1.0 (- (* (1+ i) 0.1) 0.05) 0.5 (format nil "~d" (+ 1980 i))))))
+
+  (plend)
+
+  ;; Destroy the Cario context
+  (cairo-destroy cr))
   
 (defun main ()
   (within-main-loop
@@ -66,25 +151,59 @@
 				 :title "GTK Example"))
 	  (box (make-instance 'gtk-box
 			      :orientation :vertical
-			      :spacing 0)))
+			      :spacing 0))
+	  (plot-func #'plot-curves))
 
+      ; Main window
       (gtk-container-add window box)
       (g-signal-connect window "destroy"
 			(lambda (widget)
 			  (declare (ignore widget))
 			  (leave-gtk-main)))
  
-      (create-menus window box)
+      ; Menus
+      (let ((menu-bar (make-instance 'gtk-menu-bar :expand nil))
+					;l(gtk-menu-bar-new))
+	    (plot-item (gtk-menu-item-new-with-label "Plot")))
+	(gtk-box-pack-start box menu-bar)
+	(gtk-menu-shell-append menu-bar plot-item)
+	(let ((plot-menu (gtk-menu-new)))
+	  (setf (gtk-menu-item-submenu plot-item) plot-menu)
+	  (let ((curves-item (gtk-menu-item-new-with-label "Curves"))
+		(histogram-item (gtk-menu-item-new-with-label "Histogram"))
+		(interactive-item (gtk-menu-item-new-with-label "Interactive Selection"))
+		(quit-item (gtk-menu-item-new-with-label "Quit")))
+	    (gtk-menu-shell-append plot-menu curves-item)
+	    (gtk-menu-shell-append plot-menu histogram-item)
+	    (gtk-menu-shell-append plot-menu interactive-item)
+	    (gtk-menu-shell-append plot-menu (gtk-separator-menu-item-new))
+	    (gtk-menu-shell-append plot-menu quit-item)
+	    (g-signal-connect curves-item "activate"
+			      (lambda (widget)
+				(declare (ignore widget))
+				(setf plot-func #'plot-curves)
+				(gtk-widget-queue-draw box)))
+	    (g-signal-connect histogram-item "activate"
+			      (lambda (widget)
+				(declare (ignore widget))
+				(setf plot-func #'plot-histogram)
+				(gtk-widget-queue-draw box)))
+	    (g-signal-connect quit-item "activate"
+			      (lambda (widget)
+				(declare (ignore widget))
+				(gtk-widget-destroy window))))))
 
+      ; Plotting
       (let ((plot-box (make-instance 'gtk-drawing-area
-				     :width-request 600
+				     :expand t
+				     :width-request 600 
 				     :height-request 600)))
 	(gtk-box-pack-start box plot-box)
 	(g-signal-connect plot-box "draw"
 			  (lambda (widget cr)
 			    (let ((cr (pointer cr))
 				  (plot-window (gtk-widget-window widget)))
-			      (plot plot-window cr)))))
+			      (funcall plot-func plot-window cr)))))
 
       (gtk-widget-show-all window))))
 
